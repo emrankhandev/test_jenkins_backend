63cbfb468cd50c86bc6e285a43434f9bd21596cd
emrankhnss <atdimla@gmail.com>
2023-11-11 17:50:20 +0600

    test

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping(value = "/sir")
+    public String getHello(){
+        return  "Hello Mejan Vai";
+    }
+}
